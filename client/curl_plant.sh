7aa81c7d7db610ad6fe0d2b55908fda862b0eabc
Doug Bergh <dougbergh@gmail.com>
2016-09-26 16:44:58 -0700

    Added api/... to urls

--- client/curl_plant.sh
+++ client/curl_plant.sh
@@ -1,8 +1,8 @@
 curl -X POST \
 	-H "Content-Type: application/json" \
 	-d @p1.json \
-	http://localhost:8000/plants
+	http://localhost:8000/api/plants
 curl -X POST \
 	-H "Content-Type: application/json" \
 	-d @p2.json \
-	http://localhost:8000/plants
+	http://localhost:8000/api/plants
--- client/curl_pvarray.sh
+++ client/curl_pvarray.sh
@@ -1,12 +1,12 @@
 curl -X POST \
 	-H "Content-Type: application/json" \
 	-d @pva11.json \
-	http://localhost:8000/pvarrays
+	http://localhost:8000/api/pvarrays
 curl -X POST \
 	-H "Content-Type: application/json" \
 	-d @pva12.json \
-	http://localhost:8000/pvarrays
+	http://localhost:8000/api/pvarrays
 curl -X POST \
 	-H "Content-Type: application/json" \
 	-d @pva21.json \
-	http://localhost:8000/pvarrays
+	http://localhost:8000/api/pvarrays
