22a9c9e7f89d3066efa7a67d8db14551e9f4415b
Doug Bergh <dougbergh@gmail.com>
2016-10-05 14:21:32 -0700

    Added /api/plant_count

--- server/osparc/views.py
+++ server/osparc/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
-from osparc.models import Account,UploadActivity,PlantType,Plant,PlantTimeSeries
-from osparc.serializers import AccountSerializer,UploadActivitySerializer,PlantTypeSerializer,PlantSerializer
-from osparc.serializers import PlantTimeSeriesSerializer
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import response, schemas
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from osparc.models import Account,UploadActivity,PlantType,Plant,PlantTimeSeries
+from osparc.serializers import AccountSerializer,UploadActivitySerializer,PlantTypeSerializer,PlantSerializer
+from osparc.serializers import PlantTimeSeriesSerializer
 
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -27,7 +33,21 @@ class PlantTimeSeriesViewSet(viewsets.ModelViewSet):
     queryset = PlantTimeSeries.objects.all()
     serializer_class = PlantTimeSeriesSerializer
 
+# count
+class PlantCountView(APIView):
+    def get(self, request, format=None):
+        plantDict = { 'count': Plant.objects.count() }
+        return Response(plantDict)
 
+    # def post(self,request,format=None):
+    #     serializer = SnippetSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data,status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# swagger
 @api_view()
 @renderer_classes([OpenAPIRenderer, SwaggerUIRenderer])
 def schema_view(request):
--- server/urls.py
+++ server/urls.py
@@ -1,8 +1,8 @@
+from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from osparc.views import AccountViewSet,UploadActivityViewSet,PlantTypeViewSet,PlantViewSet
 from osparc.views import PlantTimeSeriesViewSet
-from django.conf.urls import url, include
-from osparc.views import schema_view
+from osparc.views import schema_view, PlantCountView
 
 router = DefaultRouter(trailing_slash=False)
 
@@ -12,14 +12,8 @@ router.register(prefix='planttypes', viewset=PlantTypeViewSet)
 router.register(prefix='plants', viewset=PlantViewSet)
 router.register(prefix='planttimeseries', viewset=PlantTimeSeriesViewSet)
 
-urlpatterns = router.urls
-
 urlpatterns = [
+	url('^api/plant_count/',PlantCountView.as_view()),
 	url('^api/',include(router.urls)),
 	url('^docs/',schema_view)
 ]
-
-# urlpatterns = [
-#     url(r'^api/', include(router.urls)),
-#     url(r'^docs/', include('rest_framework_docs.urls'))
-# ]
