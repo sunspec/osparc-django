e083703fc2756bb8ba91d3696a02d5149f64493f
Doug Bergh <dougbergh@gmail.com>
2016-11-01 16:15:40 -0700

    Removed leftover comments; cleanup

--- server/osparc/serializers.py
+++ server/osparc/serializers.py
@@ -16,24 +16,6 @@ class PlantTypeSerializer(serializers.HyperlinkedModelSerializer):
         model = models.PlantType
         fields = ("name", "description")
 
-# class ShortListSerializerMixin(object):
-#     def get_fields(self):
-        
-#         use_list_fields = self.context['view'].action == u'list' \
-#                             and getattr(self.Meta, 'list_fields')
-#         if use_list_fields:
-#             detail_fields = self.opts.fields
-
-#             print detail_fields
-
-#             # self.fields = self.Meta.list_fields
-
-#         fields = super(ShortListSerializerMixin, self).get_fields()
-
-#         if use_list_fields:
-#             self.fields = detail_fields
-#         return fields
-
 class PlantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         depth = 1
@@ -43,44 +25,6 @@ class PlantSerializer(serializers.HyperlinkedModelSerializer):
             "latitude","longitude","timezone","weathersource","derate","arraytype","tilt","azimuth",
             "storageoriginalcapacity","storagecurrentcapacity","storagestateofcharge","accountid",
             "versioncreationtime","versionid","solaranywheresite","uploadactivity")
-
-# class PlantDetailsSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = models.Plant
-#         fields = ("description","county","city",
-#             "latitude","longitude","timezone","weathersource","derate","trackertype","tilt","azimuth",
-#             "storageoriginalcapacity","storagecurrentcapacity","storagestateofcharge","accountid",
-#             "versioncreationtime","versionid","solaranywheresite")
-
-# class PlantSummarySerializer(PlantSerializer):
-#     def __init__(self,*args,**kwargs):
-#         super(PlantSummarySerializer,self).__init__(*args,**kwargs)
-#         self.fields.pop("description")
-#         self.fields.pop("activationdate")
-#         self.fields.pop("postalcode")
-#         self.fields.pop("state")
-#         self.fields.pop("county")
-#         self.fields.pop("city")
-#         self.fields.pop("latitude")
-#         self.fields.pop("longitude")
-#         self.fields.pop("timezone")
-#         self.fields.pop("weathersource")
-#         self.fields.pop("dcrating")
-#         self.fields.pop("derate")
-#         self.fields.pop("trackertype")
-#         self.fields.pop("tilt")
-#         self.fields.pop("azimuth")
-#         self.fields.pop("storageoriginalcapacity")
-#         self.fields.pop("storagecurrentcapacity")
-#         self.fields.pop("storagestateofcharge")
-#         self.fields.pop("accountid")
-#         self.fields.pop("versioncreationtime")
-#         self.fields.pop("versionid")
-#         self.fields.pop("solaranywheresite")
-
-#     class Meta:
-#         model = models.Plant
-#         fields = ("plantuuid","name")
 
 class PlantTimeSeriesSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
@@ -107,23 +51,3 @@ class ReportRunSerializer(serializers.ModelSerializer):
         model = models.ReportRun
         fields = ("id","status","reportdefinition","runsubmittime","runstarttime","runcompletetime","firstmeasurementdate",
                 "lastmeasurementdate","numberofmeasurements","numberofplants","totaldccapacity","totalstoragecapacity","kpis")
-
-
-
-# class KPISerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = models.KPI
-#         fields = ("dcratingplants","dcratingfirstday","dcratinglastday","dcratingmean","dcratingmed","dcratingmin","dcratingmax",
-#                   "insolplants","insolfirstday","insollastday","insolmean","insolmed","insolmin","insolmax",
-#                   "generatedplants","generatedfirstday","generatedlastday","generatedmean","generatedmed","generatedmin","generatedmax",
-#                   "yieldplants","yieldfirstday","yieldlastday","yieldmean","yieldmed","yieldmin","yieldmax",
-#                   "prplants","prfirstday","prlastday","prmean","prmed","prmin","prmax",
-#                   "storcapplants","storcapfirstday","storcaplastday","storcapmean","storcapmed","storcapmin","storcapmax",
-#                   "storsohplants","storsohfirstday","storsohlastday","storsohmean","storsohmed","storsohmin","storsohmax",
-#                 )
-
-
-# class TotalSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = models.Total
-#         fields = ("totaldcrating","totalstoragecapacity")
